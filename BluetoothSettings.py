921bc09da69e536fe56e20eec151c6bc36e2413a
tornandbroken <daniel.gisenstein@protonmail.com>
2025-07-21 11:20:17 +0000

    Add files via upload

--- BluetoothSettings.py
+++ BluetoothSettings.py
@@ -2,46 +2,52 @@
 
 import os
 import sys
+import subprocess
 
 if not os.geteuid() == 0:
-    sys.exit("Only root can run this script")
+    sys.exit(" : : : Only root can run this script  : : : : : : : : : : : : : : : : : : : : : :")
+
 
 def menue():
     print("")
     print("")
-    print("	[ 1 ]	[ Create a backup of your Bluetooth settings ]")
+    print("   1 󰉙  Create a backup of your Bluetooth settings")
     print("")
-    print("		- Please check your files next to this Python script -")
-    print("")
-    print("")
-    print("	[ 2 ]	[ Install that backup !! Your System will reboot immediately !! ]")
-    print("")
-    print("		-  Make sure that the Bluetooth backup files are next to this program")
+    print(" - - -  Before running this script set up all your Bluetooth devices - - - - - - - ")
     print("")
     print("")
-    print("	[ 3 ]	[ Reset your Bluetooth settings. !! Each device will be removed !! ]")
+    print("   2 󰉍  Installing the backup   󱈸 System will reboot immediately 󱈸 ")
     print("")
-    print("		- Use that only if you have messed up your Bluetooth settings -")
-    print("")
-    print("")
-    print("	[ 4 ]	[ Exit ]")
-    print("")
-    print("		- Leave this script -")
+    print(" - - -  Make sure that your Bluetooth backup files are next to this program  - - -")
     print("")
     print("")
-    auswahl = input("Choose an Option ")
+    print("   3 󰉘  Reset your Bluetooth settings   󱈸 Each device will be removed 󱈸")
+    print("")
+    print(" - - -  Use that only if you have messed up your Bluetooth settings  - - - - - - -")
+    print("")
+    print("")
+    print("   4 󰉒  Exit")
+    print("")
+    print(" - - -  Leave this script  - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("")
+
+    auswahl = input("  ")
+
+    print("")
+
     if auswahl == "1":
-        os.system('cp -p -r /var/lib/bluetooth bluetooth && chmod -R 555 bluetooth')
+        os.system('cp -p -r /var/lib/bluetooth bluetooth && chmod -R 555 bluetooth');
+        print(" + + +  Backup 󰉗 bluetooth created in", os.getcwd(), " + + + + + + +")
     elif auswahl == "2":
         os.system('rsync -au bluetooth /var/lib && reboot')
     elif auswahl == "3":
-        os.system('rm -rf /var/lib/bluetooth')
+        os.system('rm -rf /var/lib/bluetooth');
+        print(" x x x  Bluetooth removed  󰉘   x x x x x x x x x x x x x x x x x x x x x x x x x x")
     elif auswahl == "4":
-        os.system("exit")
+        os.system("exit");
+        print(" : : :  Goodbye  : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : :")
     else:
-        print("invalid section")
+        print(" # # #  Invalid  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #")
 
 menue()
-
-
 
